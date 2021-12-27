56b0e008495433bed242280ba18c55333699fda3
Manivannan Sengodan <manidkn@gmail.com>
2021-12-26 17:58:44 -0800

    More refactoring

---
+++ DataModel/Shared/Constants.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace DataModel.Shared
+{
+    public static class Constants
+    { 
+        public static class Auth
+        {
+            public static readonly string PhoneNumber = "phone_number";
+            public static readonly string Header = "Authorization";
+            public static readonly string Spliter = " ";
+            public static readonly List<string> anonymousAllowList 
+                = new List<string>() {"/ServiceProviders HTTP: GET"};
+
+        }
+        public static class MongoDB
+        {
+            public static string ServiceProvideCollection = "ServiceProviders";
+            public static string CustomerCollection = "Customers";
+            public static string MedicineAutoFillCollection = "Medicines";
+            public static string ServiceProviderCategoriesCollection = "ServiceProviderCategories";
+            public static string BlockedUsersCollection = "BlockedUsers";
+            public static string TestUsersCollection = "TesterUsers";
+            public static string OrganisationCollection = "Organisations";
+            public static string ServiceProviderCreatedTemplatesCollection = "ServiceProviderCreatedTemplates";
+        }
+        public static class Firebase
+        {
+            public  static string FirebaseProjectId = "ds-connect";
+
+        }
+    }
+}
---
+++ DataModel/Shared/NDConfiguration.cs
@@ -0,0 +1,39 @@
+﻿namespace DataModel.Shared
+{
+    public class MongoConfiguration
+    {
+        public string DatabaseName { get; set; }
+        public string ConnectionString { get; set; }
+            
+    }
+    public class TwillioConfiguration
+    {
+        public string AccountSid { get; set; }
+        public string ApiKey { get; set; }  
+        public string ApiSecret { get; set; }
+
+    }
+    public class FCMConfiguration
+    {
+        public string ServerKey { get; set; }   
+        public string SenderId { get; set; }
+
+    }
+
+    public class APNConfiguration
+    {
+        public int ServiceType { get; set; }
+        public string BundleId { get; set; }
+        public string P8PrivateKey { get; set; }
+        public string P8PrivateKeyId { get; set; }
+        public string TeamId { get; set; }
+
+    }
+
+    public class BlobStorageConfiguration
+    {
+        public string ConnectionString { get; set; }
+
+    }
+
+}
