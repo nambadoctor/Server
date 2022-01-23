84e090e59c25af12cc568163cb1cb9017382efe7
suryam0801 <58582997+suryam0801@users.noreply.github.com>
2022-01-22 20:44:31 -0800

    minor

--- DataLayer/Context/MongoContext.cs
+++ DataLayer/Context/MongoContext.cs
@@ -52,7 +52,7 @@ namespace MongoDB.GenericRepository.Context
             // Configure mongo (You can inject the config, just to simplify)
             MongoClient = new MongoClient("mongodb://ndproduction:fcPK6mih6BsfbI5myOEahSqnGZmudxsNhmUa08axzmJU6OABa0aUZEgB2w9SOaydcl0CB90CUUxAack56ZzlfA==@ndproduction.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@ndproduction@");
 
-            Database = MongoClient.GetDatabase("NambaDoctor");
+            Database = MongoClient.GetDatabase("NambaDoctor"); 
         }
 
         public IMongoCollection<T> GetCollection<T>(string name)
