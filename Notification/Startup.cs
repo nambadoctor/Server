4718d2c6745d02aa4908104026876fa36870151c
Jacob Abraham <43879655+jacob-abe@users.noreply.github.com>
2022-02-17 11:52:03 +0530

    Undo revert

---
+++ Notification/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.GenericRepository.Context;
+using MongoDB.GenericRepository.Interfaces;
+using MongoDB.GenericRepository.Repository;
+using MongoDB.GenericRepository.UoW;
+using Notification.Mode.SMS;
+
+[assembly: FunctionsStartup(typeof(Notification.Function.Startup))]
+namespace Notification.Function
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            Console.WriteLine("FunctionsStartup called");
+            builder.Services.AddScoped<IMongoContext, MongoContext>();
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            builder.Services.AddScoped<ISmsService, SmsService>();
+            var testSms = Environment.GetEnvironmentVariable("sms_test");
+            builder.Services.AddScoped<ISmsRepository, SmsRepository>((repository) =>
+            {
+                return new SmsRepository(testSms != null ? bool.Parse(testSms) : true);
+            });
+
+            builder.Services.AddScoped<IServiceProviderRepository, ServiceProviderRepository>();
+            builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+            builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+        }
+    }
+}
