416dd10325f975cc6eb0483e7aaaac8a13d835a9
Jacob Abraham <43879655+jacob-abe@users.noreply.github.com>
2022-03-08 11:17:39 +0530

    Revert "Merge pull request #44 from nambadoctor/fix/reminder_behaviour"
    This reverts commit 57c04ff1bfd4e8a39984692b702c5c4effc6b2da, reversing
    changes made to ed239ef54cdf4c4a064c9821c3f39cdf31a75830.

---
+++ DataLayer/MongoDBInstance.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Driver;
+using DataModel.Mongo;
+using DataModel.Shared;
+
+namespace DataLayer
+{
+    public sealed class MongoDBInstance
+    {
+        IMongoDatabase mongoDb = null;
+        MongoClient mongoClient = null;
+
+        private static readonly object padlock = new object();
+        private static MongoDBInstance mongoDbInstance = null;
+
+        public static MongoDBInstance Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (mongoDbInstance == null)
+                    {
+                        mongoDbInstance = new MongoDBInstance();
+                    }
+                    return mongoDbInstance;
+                }
+            }
+        }
+        public MongoDBInstance()
+        {
+            mongoClient = new MongoClient(ConnectionConfiguration.MongoConnectionString);
+            this.mongoDb = mongoClient.GetDatabase(ConnectionConfiguration.MongoDatabaseName);
+        }
+
+        public IMongoDatabase GetMongoDB()
+        {
+            return this.mongoDb;
+        }
+
+        public MongoClient GetMongoClient()
+        {
+            return this.mongoClient;
+        }
+    }
+}
